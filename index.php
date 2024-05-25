6d74e5ad6a5151f3452708fc7eed83357d586a32
akash-1191 <158453744+akash-1191@users.noreply.github.com>
2024-05-25 19:45:25 +0530

    make protfolio

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>particles.js</title>
+  <meta name="description" content="particles.js is a lightweight JavaScript library for creating particles.">
+  <meta name="author" content="Vincent Garreau" />
+  <meta name="viewport"
+    content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+  <link rel="stylesheet" media="screen" href="demo.css">
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+  <!-- particles.js container -->
+  <section id="particles-js" class="relative">
+    <div class="flex justify-center items-center">
+      <div>
+        <h2 class="absolute  w-full h-full text-white text-2xl ">
+          hello how are you</h2>
+      </div>
+      <div>
+        <img src="./image/akashpro.jpeg" alt="" class=" absolute " >
+      </div>
+    </div>
+  </section>
+
+
+
+
+
+</body>
+<!-- scripts -->
+<script src="particles.js"></script>
+<script src="./js/app.js"></script>
+
+</html>
