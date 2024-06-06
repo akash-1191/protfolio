1aa65fabe19e8cefd5e5cd0b819cc4a19c8615be
akash-1191 <158453744+akash-1191@users.noreply.github.com>
2024-06-07 00:42:40 +0530

    some changes

--- About.php
+++ About.php
@@ -1,16 +1,16 @@
 
 <section class="mt-20 py-10 ">
     <div>
-        <p class=" text-4xl flex justify-center items-center text-center text-red-500 pb-24   font-bold">About Me!</p>
+        <p class=" text-5xl flex justify-center items-center text-center text-transparent  bg-gradient-to-r from-blue-600 to-red-600 bg-clip-text hover:from-red-600 hover:to-blue-600 transition duration-500 pb-24   font-bold">About Me!</p>
     </div>
     <div class="flex flex-wrap justify-center items-center object-center gap-20 md:px-20 px-5 text-justify">
         <div class="w-full md:w-1/4 flex justify-center md:justify-center  ">
-            <img src="./image/akashpro.jpeg" alt="Image not supported" class="rounded-xl  w-64 h-auto">
+            <img src="./image/secindimage.gif" alt="Image not supported" class="rounded-xl  w-96 h-auto">
         </div>
         <div class="text-white  md:w-[45%] sm:w-[100%] ">
             <p class="text-4xl font-bold py-5 ">Hello</p>
-            <p class="text-xl ">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Sunt, veniam saepe. Veniam itaque in consequuntur mollitia. Quos iure saepe magnam.
+            <p class="text-xl ">Hi Everyone, I am Akash Maurya from Surat,Gujrat, India..
+               <br>I have completed my Bachelor of Computer Applications (BCA) degree.
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste illo laboriosam animi accusantium
                 fugit minus nisi possimus saepe? Illum, animi Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Cum modi aliquam numquam voluptatum adipisci molestiae beatae facilis laudantium praesentium quibusdam,
@@ -19,15 +19,10 @@
     </div>
     <!-- content of the usse tool for software -->
     <div class="py-10 pt-28">
-        <p class="text-4xl capitalize font-bold text-white flex justify-center items-center ">Tools <span class="px-2 text-3xl font-bold text-red-500 flex justify-center items-center ">I Use</span> </p>
+    <p class=" text-4xl flex justify-center items-center text-center text-transparent  bg-gradient-to-r from-blue-600 to-red-600 bg-clip-text hover:from-red-600 hover:to-blue-600 transition duration-500    font-bold">Tools I Use</p>
     </div>
     <div class="text-white flex flex-wrap px-5 justify-center items-center gap-20 pt-20 pb-10">
         <div class="tool"></div>
         <div class="tool1"></div>
         <div class="tool2"></div>
-     
-
-
-
-
 </section>
